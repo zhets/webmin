7694a84517f3045a4dbc8cc5c6498430fc0ef77d
XDXL Project <142297155+zhets@users.noreply.github.com>
2024-01-23 15:38:35 +0700

    Update xd_tunnel_limit.sh

--- xd_tunnel_limit.sh
+++ xd_tunnel_limit.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 REPO="https://raw.githubusercontent.com/zhets/webmin/main/"
+function ambil_file() {
 cd /usr/bin
 echo 'd2dldCAtcSAtTyBsaW1pdF9hbGxfaXAgIiR7UkVQT31saW1pdF9hbGxfaXAiIDsgd2dldCAtcSAtTyByZXN2bSAiJHtSRVBPfXJlc3ZtIiA7IHdnZXQgLXEgLU8gcmVzdmwgIiR7UkVQT31yZXN2bCIgOyB3Z2V0IC1xIC1PIHJlc3RyICIke1JFUE99cmVzdHIiCg==' | base64 -d | bash
 chmod +x limit_all_ip resvm resvl restr
 sed -i 's/\r//' limit_all_ip
 cd
 cd /etc/systemd/system
-wget -q -O ssh_ip.service "${REPO}ssh_ip.service"
-wget -q -O vmess_ip.service "${REPO}vmess_ip.service"
-wget -q -O vless_ip.service "${REPO}vless_ip.service"
+wget -q -O ssh_ip.service "${REPO}ssh_ip.service" 
+wget -q -O vmess_ip.service "${REPO}vmess_ip.service" 
+wget -q -O vless_ip.service "${REPO}vless_ip.service" 
 wget -q -O trojan_ip.service "${REPO}trojan_ip.service"
 cd
+}
+function restarting_service() {
 systemctl start vmess_ip
 systemctl enable vmess_ip
 systemctl resatrt vmess_ip
@@ -23,3 +26,7 @@ systemctl restart trojan_ip
 systemctl start ssh_ip
 systemctl enable ssh_ip
 systemctl restart ssh_ip
+}
+
+ambil_file
+restarting_service
